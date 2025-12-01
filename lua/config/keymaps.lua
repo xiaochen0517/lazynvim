9ca7c24c96ffdb20ac7be7aae878b7d648977acc
mochen <2827075398@qq.com>
2025-12-01 21:39:27 +0800

    feat: 重置配置

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,15 @@
 vim.keymap.set("n", "<leader>tt", ":tabnew | terminal<CR>", { desc = "Open terminal with new tab" })
 vim.keymap.set("i", "<C-l>", "<End>", { desc = "Move cursor to end" })
 vim.keymap.set("i", "jk", "<ESC>", { noremap = true, silent = true, desc = "Esc insert mode" })
+
+-- Debug keymaps
+-- run to cursor
+vim.keymap.set("n", "<F6>", "<cmd>lua require'dap'.run_to_cursor()<CR>", { desc = "DAP: Run to Cursor" })
+-- step into
+vim.keymap.set("n", "<F7>", "<cmd>lua require'dap'.step_into()<CR>", { desc = "DAP: Step Into" })
+-- step over
+vim.keymap.set("n", "<F8>", "<cmd>lua require'dap'.step_over()<CR>", { desc = "DAP: Step Over" })
+-- continue
+vim.keymap.set("n", "<F9>", "<cmd>lua require'dap'.continue()<CR>", { desc = "DAP: Continue" })
+-- toggle breakpoint
+vim.keymap.set("n", "<F10>", "<cmd>lua require'dap'.toggle_breakpoint()<CR>", { desc = "DAP: Toggle Breakpoint" })
